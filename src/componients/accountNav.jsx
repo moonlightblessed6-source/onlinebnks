cc45b8f9da213bd8efa80a74b818d757fc14c366
Moonlight Blessed <moonlightblessed6@gmail.com>
2026-01-17 12:51:02 -0500

    first commit

--- src/componients/accountNav.jsx
+++ src/componients/accountNav.jsx
@@ -1,96 +1,96 @@
-// import React, {useState,useEffect} from 'react'
-// import logos from '../assets/logo/logos.png'
-// import {Accountnavbar} from './styled'
-// import { MdAccountCircle } from "react-icons/md";
-// import { GiHamburgerMenu } from "react-icons/gi";
-// import { Link, useNavigate } from 'react-router-dom';
-// import DashboardButton from './DashboardButton'
+import React, {useState,useEffect} from 'react'
+import logos from '../assets/logo/logos.png'
+import {Accountnavbar} from './styled'
+import { MdAccountCircle } from "react-icons/md";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Link, useNavigate } from 'react-router-dom';
+import DashboardButton from './DashboardButton'
 
 
 
-// const AccountNav = ({dropsidebar }) => {
+const AccountNav = ({dropsidebar }) => {
 
-//   const [name, setName] = useState('');
+  const [name, setName] = useState('');
 
-//   const [droplogout, setDropLogout] = useState(false)
+  const [droplogout, setDropLogout] = useState(false)
 
-//   const accounprve = () => {
-//     setDropLogout(prev => !prev)
-//   }
+  const accounprve = () => {
+    setDropLogout(prev => !prev)
+  }
 
-//  const navigate = useNavigate();
+ const navigate = useNavigate();
 
-//   const handlelogout = () => {
-//     localStorage.removeItem('authToken')
-//      navigate('/login')
-//   }
+  const handlelogout = () => {
+    localStorage.removeItem('authToken')
+     navigate('/login')
+  }
 
  
  
 
-//   useEffect(() => {
-//       const fetchDashboard = async () => {
-//         try {
-//           const token = localStorage.getItem('authToken');
-//           if (!token) {
-//             console.error('No auth token found');
-//             return;
-//           }
+  useEffect(() => {
+      const fetchDashboard = async () => {
+        try {
+          const token = localStorage.getItem('authToken');
+          if (!token) {
+            console.error('No auth token found');
+            return;
+          }
   
-//           const response = await fetch('http://127.0.0.1:8000/api/account/dashboard', {
-//             headers: {
-//               'Authorization': `Token ${token}`
-//             }
-//           });
+          const response = await fetch('http://127.0.0.1:8000/api/account/dashboard', {
+            headers: {
+              'Authorization': `Token ${token}`
+            }
+          });
   
-//           if (!response.ok) {
-//             throw new Error('Failed to fetch dashboard data');
-//           }
+          if (!response.ok) {
+            throw new Error('Failed to fetch dashboard data');
+          }
   
-//           const data = await response.json();
+          const data = await response.json();
   
-//           const firstName = data.account?.first_name || '';
-//           const lastName = data.account?.last_name || '';
-//           const fullName = `${firstName} ${lastName}`.trim();
+          const firstName = data.account?.first_name || '';
+          const lastName = data.account?.last_name || '';
+          const fullName = `${firstName} ${lastName}`.trim();
   
-//           setName(fullName);
-//         } catch (error) {
-//           console.error(error);
-//         }
-//       };
+          setName(fullName);
+        } catch (error) {
+          console.error(error);
+        }
+      };
   
-//       fetchDashboard();
-//     }, []);
+      fetchDashboard();
+    }, []);
 
 
    
 
-//   return (
+  return (
     
-//     <Accountnavbar>
+    <Accountnavbar>
       
-//       <div className="accountNavWrapper">
-//         <div className="left"><img src={logos} alt="logo" /></div>
-//             <div onClick={dropsidebar} className='menubardrop'>
-//       <GiHamburgerMenu style={{ color: '#fff', fontSize: '24px' }} />
-//     </div>
+      <div className="accountNavWrapper">
+        <div className="left"><img src={logos} alt="logo" /></div>
+            <div onClick={dropsidebar} className='menubardrop'>
+      <GiHamburgerMenu style={{ color: '#fff', fontSize: '24px' }} />
+    </div>
 
-//         <div className="rigt">
+        <div className="rigt">
          
-//             <a onClick={accounprve} style={{fontSize: '20px', color: 'white'}} id='name'>
-//             <strong></strong> <span> {name || 'Loading...'} <MdAccountCircle style={{fontSize: '27px'}}  /></span>
-//           </a>
-//         </div>
-//       </div>
-//      {droplogout && (
-//     <div className='droplogou'>
-//        <span onClick={() => window.location.reload()}>Account</span>
-//        <span onClick={handlelogout}>Logout</span>
-//       </div>
-//       )}
+            <a onClick={accounprve} style={{fontSize: '20px', color: 'white'}} id='name'>
+            <strong></strong> <span> {name || 'Loading...'} <MdAccountCircle style={{fontSize: '27px'}}  /></span>
+          </a>
+        </div>
+      </div>
+     {droplogout && (
+    <div className='droplogou'>
+       <span onClick={() => window.location.reload()}>Account</span>
+       <span onClick={handlelogout}>Logout</span>
+      </div>
+      )}
       
-//     </Accountnavbar>
-//   )
-// }
+    </Accountnavbar>
+  )
+}
 
-// export default AccountNav
+export default AccountNav
